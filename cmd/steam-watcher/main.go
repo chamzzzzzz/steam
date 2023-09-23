00d43094d2394bf214b6b2898ab71883f6e7d8d0
Liu Chen <chamzzzzzz@gmail.com>
2023-09-23 10:14:31 +0800

    merge

--- cmd/steam-watcher/main.go
+++ cmd/steam-watcher/main.go
@@ -10,30 +10,93 @@ import (
 	"github.com/chamzzzzzz/steam"
 )
 
+var (
+	file = "applist.csv"
+)
+
 func main() {
+	apps, err := read(file)
+	if err != nil {
+		log.Fatalf("read file fail. err='%v'", err)
+	}
+
 	client := steam.Client{}
 	p, err := client.RequestGetAppList()
 	if err != nil {
 		log.Fatalf("request app list fail. err='%v'", err)
 	}
 
-	sort.Slice(p.AppList.Apps, func(i, j int) bool {
-		return p.AppList.Apps[i].AppID < p.AppList.Apps[j].AppID
+	apps = merge(apps, p.AppList.Apps)
+	sort.Slice(apps, func(i, j int) bool {
+		return apps[i].AppID < apps[j].AppID
 	})
 
-	file := "applist.csv"
+	if err := write(file, apps); err != nil {
+		log.Fatalf("write file fail. err='%v'", err)
+	}
+	log.Printf("write file success. file='%s'", file)
+}
+
+func write(file string, apps []*steam.App) error {
 	f, err := os.Create(file)
 	if err != nil {
-		log.Fatalf("create file fail. err='%v'", err)
-		return
+		return err
 	}
 	defer f.Close()
 
 	writer := csv.NewWriter(f)
 	defer writer.Flush()
 	writer.Write([]string{"AppID", "Name"})
-	for _, app := range p.AppList.Apps {
-		writer.Write([]string{strconv.Itoa(app.AppID), app.Name})
+	for _, app := range apps {
+		if err := writer.Write([]string{strconv.Itoa(app.AppID), app.Name}); err != nil {
+			return err
+		}
 	}
-	log.Printf("write file success. file='%s'", file)
+	return nil
+}
+
+func read(file string) ([]*steam.App, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	apps := make([]*steam.App, 0, len(records))
+	for _, record := range records[1:] {
+		appID, err := strconv.Atoi(record[0])
+		if err != nil {
+			return nil, err
+		}
+		apps = append(apps, &steam.App{
+			AppID: appID,
+			Name:  record[1],
+		})
+	}
+	return apps, nil
+}
+
+func merge(apps1, apps2 []*steam.App) []*steam.App {
+	m := make(map[int]*steam.App)
+	for _, app := range apps1 {
+		m[app.AppID] = app
+	}
+	for _, app2 := range apps2 {
+		app1 := m[app2.AppID]
+		if app1 == nil {
+			apps1 = append(apps1, app2)
+		} else {
+			app1.Name = app2.Name
+		}
+	}
+	return apps1
 }
